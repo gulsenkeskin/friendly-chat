a3d5e1633ab4a1109e3a357f4d01a6073d24ead8
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-01-02 16:00:53 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    FriendlyChatApp(),
+  );
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'FriendlyChat',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('FriendlyChat'),
         ),
       ),
-    ),
-  );
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  const ChatScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("FriendlyChat"),
+      ),
+    );
+  }
 }
